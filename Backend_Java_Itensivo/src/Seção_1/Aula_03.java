ea5acf0add81c5c4c5cb41ff12feb1ee10adaf65
renatof4rias <renatof4rias@gmail.com>
2024-02-28 20:16:23 -0300

    Aula 3

---
+++ Backend_Java_Itensivo/src/Seção_1/Aula_03.java
@@ -0,0 +1,15 @@
+package Seção_1;
+
+public class Aula_03 {
+
+	//Regras de Declaração para arquivos fontes
+	
+	// o nome do ARQUIVO criado a ESQUERDA <-------- tem que ter EXATAMENTE o mesmo NOME da classe 
+	// a diferença entre eles é no arquivo é acrescido um ".java" ao final 
+	
+	
+	// a esquerda está Aula_03.java, acima está Aula_03.
+	// qual quer mudança em um dos dois ocorre erro
+	
+	
+}
