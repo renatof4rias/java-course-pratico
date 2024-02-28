ab57fd9d90c27be1836cd72c3605ec1d800e5d9f
renatof4rias <renatof4rias@gmail.com>
2024-02-28 20:11:05 -0300

    Aula 2

---
+++ Backend_Java_Itensivo/src/Seção_1/Aula_02.java
@@ -0,0 +1,38 @@
+package Seção_1;
+
+public class Aula_02 {
+	
+	//Boas Praticas / Convenções de Código Java
+	
+	
+	// Padrao CamelCase= Consiste em não utilizar espaço em palavras compostas e utilizar letras maiusculas para separa-las	// Exemplo	
+	// velocidadeMaxima;
+	// euSouProgramador;
+
+	// Classe se utiliza padrao CamelCase iniciando com a primeira letra maiusculas	
+	// public class Pessoa
+	// public class CalculoImposto
+	// public class AutenticarCpf	
+
+	// Interface se utiliza padrao CamelCase iniciando com a primeira letra maiusculas	
+	// public interface Pessoa
+	// public interface CalculoImposto
+	// public interface AutenticarCpf
+
+	// Metodos se utiliza CamelCase iniciando com a primeira letra minuscula
+	// public void setCPF(String cpf) {}
+	// public String getCPF() {} 
+	// public String toString() {}
+	
+	// Variaveis se utiliza CamelCase iniciando com a primeira letra minuscula
+	// private String nome
+	// private String cpf
+	// private Double salario
+	// private Integer idade
+	
+	// Constante nao utiliza CamelCase, mas utiliza o urnderscore "_" como separador de palavras e sua estrutura toda maiusculas
+	// final private Integer VELOCIDADE_MAXIMA
+	// final private Double VALOR_DESCONTO
+	// final private Integer IDADE_MINIMA
+	
+}
