9c2cb7e3c91616197c57b3b0ee83f2a86005f473
renatof4rias <renatof4rias@gmail.com>
2024-02-28 20:25:09 -0300

    Aula 4

---
+++ Backend_Java_Itensivo/src/Seção_1/Aula_04.java
@@ -0,0 +1,19 @@
+package Seção_1;
+
+public class Aula_04 {
+
+	// Lab 01 
+	
+	// Pacotes sao declarados com dominions invertidos  exemplo  com.softelse.controlelogin 
+	// A "ideia" é evitar conflitos em pacotes e ter sua responsabilidades autodeclaradas 
+	
+	String nomeDoCliente;  // VARIAVEL
+	
+	static final String NOME_DO_CLIENTE = " ";  // CONSTANTE
+	
+	
+	public void calcularSalario() {
+		// METODO
+	}
+	
+}
