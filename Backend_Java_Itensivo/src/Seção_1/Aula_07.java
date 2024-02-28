dcd6fb11db99eecd4767bc73d0d245692b19500c
renatof4rias <renatof4rias@gmail.com>
2024-02-28 20:56:27 -0300

    Aula 7

---
+++ Backend_Java_Itensivo/src/Seção_1/Aula_07.java
@@ -0,0 +1,17 @@
+package Seção_1;
+
+public class Aula_07 {
+
+	//Classe Object é a RAIZ de todas as outras classes criadas
+	//Ela é Herdada automaticamente e se tornando a SUPERCLASS
+	
+	// Contem metodos para serem sobrescritos nas classes filhas 
+	
+	// toString
+	// equals
+	// hashCode
+	// getClass
+	// ...
+	
+	
+}
