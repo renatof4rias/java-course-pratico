d709a3eb49bf4fd5bbc3c8f77ae136e77b441889
renatof4rias <renatof4rias@gmail.com>
2024-02-28 22:34:04 -0300

    Aula 9

---
+++ Backend_Java_Itensivo/src/Seção_1/Aula_09.java
@@ -0,0 +1,22 @@
+package Seção_1;
+
+public class Aula_09 {
+
+	// METODO MAIN 
+	
+	// METODO RESPONSAVEL POR INICIALIZAR O SISTEMA, PONTO DE PARTIDA NA EXECUÇÃO DE UM SISTEMA
+	// SEM ELE NAO EXECUTA NADA 
+	
+	
+	// DIGITANDO psvm + ctrl + espaço O METODO É GERADO AUTOMAITCAMENTE 
+	// OU DIGITANDO main + ctrl + espaço O METODO É GERADO AUTOMAITCAMENTE 
+	
+	// SÓ PODE TER APENAS UM METODO MAIN POR SISTEMA 
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
