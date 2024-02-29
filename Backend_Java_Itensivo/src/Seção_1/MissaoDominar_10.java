d444961f1293a057ba32c4447a7941966c9c9899
renatof4rias <renatof4rias@gmail.com>
2024-02-29 19:45:38 -0300

    Missao Dominar 10

--- Backend_Java_Itensivo/src/Seção_1/MissaoDominar_10.java
+++ Backend_Java_Itensivo/src/Seção_1/MissaoDominar_10.java
@@ -10,18 +10,17 @@ public class MissaoDominar_10 {
 
 		System.out.print("Quantos Funcionarios ? ");
 		int totalFuncionarios = sc.nextInt();
-		
-		
+
 		double somaSalarios = 0.0;
 		for (int i = 0; i < totalFuncionarios; i++) {
-			System.out.print("Informe o Salario do " + (i+1) + "o Funcionario: ");
+			System.out.print("Informe o Salario do " + (i + 1) + "o Funcionario: ");
 			somaSalarios += sc.nextDouble();
 		}
-		
+
 		double mediaSalarios = somaSalarios / totalFuncionarios;
-		
+
 		System.out.println("Media Salarial da Empresa: R$" + mediaSalarios);
-		
+
 		sc.close();
 	}
 
