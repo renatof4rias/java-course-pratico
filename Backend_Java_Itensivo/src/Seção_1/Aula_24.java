e3ee2e1bba0c689598c4c68c066cddb5e8ddab0d
renatof4rias <renatof4rias@gmail.com>
2024-02-29 17:24:12 -0300

    Aula 24

---
+++ Backend_Java_Itensivo/src/Seção_1/Aula_24.java
@@ -0,0 +1,38 @@
+package Seção_1;
+
+import java.util.Scanner;
+
+public class Aula_24 {
+	
+	public static void main(String[] args) {
+
+		//Scanner  utilizado para ler dados diretamente do console
+		
+		
+		Scanner sc = new Scanner(System.in);
+		//Zona de leitura
+		//TIPOS DE LEITURA
+		/*
+		INT sc.nextInt();
+		DOUBLE sc.nextDouble();
+		CHAR sc.next().charAt(0);
+		String sc.next();          ler até o primeiro espaço em branco 
+		String sc.nextLine();	   ler até o inicio da proxima linha
+		INT sc.nextInt();
+		INT sc.nextInt();
+		
+		
+		
+		
+		
+		
+		
+		*/
+		
+		
+		sc.close();
+		
+		//Zona sem Leitura
+		
+	}
+}
