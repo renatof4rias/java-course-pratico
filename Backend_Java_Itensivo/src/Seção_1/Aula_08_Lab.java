7b06a5781ac0af8bc9e09fc2608fc8903a7d8930
renatof4rias <renatof4rias@gmail.com>
2024-02-28 22:27:28 -0300

    Aula 8

---
+++ Backend_Java_Itensivo/src/Seção_1/Aula_08_Lab.java
@@ -0,0 +1,29 @@
+package Seção_1;
+
+public class Aula_08_Lab extends Object{
+	
+	//NAO HÁ NECESSIDADE DE UTILIZAR O "EXTENDS" É FEITO DE FORMA AUTOMATICA
+	
+	
+	//METODOS DA CLASSE OBJECT QUE HERDAMOS PARA SOBREESCREVER
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+	
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+	
+}
