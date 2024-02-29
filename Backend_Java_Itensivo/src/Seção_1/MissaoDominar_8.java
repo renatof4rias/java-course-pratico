89e3ce592bb9384021d4b483b0bd3eacc5f8d89a
renatof4rias <renatof4rias@gmail.com>
2024-02-29 19:35:22 -0300

    Missao Dominar 8

---
+++ Backend_Java_Itensivo/src/Seção_1/MissaoDominar_8.java
@@ -0,0 +1,26 @@
+package Seção_1;
+
+import java.util.Scanner;
+
+public class MissaoDominar_8 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Digite o Valor da Base: ");
+		double a = sc.nextDouble();
+
+		System.out.print("Digite o Valor da Altura: ");
+		double b = sc.nextDouble();
+
+		double area = a * b;
+		double perimetro = 2 * (a + b);
+
+		System.out.println("Area do Retangulo: " + area);
+		System.out.println("Area do Perimetro: " + perimetro);
+		
+		sc.close();
+	}
+
+}
