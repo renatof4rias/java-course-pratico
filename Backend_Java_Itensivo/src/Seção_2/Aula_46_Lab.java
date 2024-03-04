a3479a09c35c81f0d3c1fcc81de2a793d1d422c4
renatof4rias <renatof4rias@gmail.com>
2024-03-04 17:16:55 -0300

    Aula 46

---
+++ Backend_Java_Itensivo/src/Seção_2/Aula_46_Lab.java
@@ -0,0 +1,32 @@
+package Seção_2;
+
+public class Aula_46_Lab {
+
+	// VAR - Inferencia de tipo 
+	
+	// Tipo coringa, para armazenar valores
+	// Sua utilização em Variaveis Locais 
+	// Precisa ser Inicializada 
+	// Seu tipo é fixo
+	// Uso em Atributos e Parametros nao sao permitidos
+	// Seu tipo é atribuido com inferencia 
+	
+	@SuppressWarnings("unused")
+	public void asasfas() {
+		 var idade = 0;
+		 var nome = "dez";
+		 var sexo = 'd';
+		 var altura = 1.75;
+
+		 
+		 
+	}
+	
+	
+	
+	
+	
+	
+	
+}
+   
