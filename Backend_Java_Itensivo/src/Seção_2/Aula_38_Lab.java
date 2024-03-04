5ea7fe9669291ec4e9cf00ebd18432626ea7336f
renatof4rias <renatof4rias@gmail.com>
2024-03-04 11:25:07 -0300

    Aula 39

---
+++ Backend_Java_Itensivo/src/Seção_2/Aula_38_Lab.java
@@ -0,0 +1,29 @@
+package Seção_2;
+
+public class Aula_38_Lab {
+
+	@SuppressWarnings("removal")
+	Integer in = new Integer(10);
+	// Atribuição via construtor dá mais "Liberdade" em aplicar um logica de Inicialização
+	
+	Integer i = 10;
+	// Atribuição via AUTOBOXING, mais clean e nao utiliza construtor de forma explicita
+	
+	
+	Integer n = Integer.valueOf(10);
+	//Atribuição via metodo
+	
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		
+		int in = 10;
+		Integer inV = Integer.valueOf(in);
+		
+		
+		
+		
+		
+		
+	}
+	
+}
---
+++ Backend_Java_Itensivo/src/Seção_2/Aula_39.java
@@ -0,0 +1,34 @@
+package Seção_2;
+
+public class Aula_39 {
+
+	// ValueOf  ->  Primitivo para Wrapper
+	// Textos e Primitivos
+	
+	int a = 10; // <- Primitivo
+	Integer a1 = Integer.valueOf(a);  //PRIMITIVO PARA WRAPPER
+
+	String b = "123";
+	Integer b1 = Integer.valueOf(b);  // STRING PARA WRAPPER NUMERICO
+
+	// CASO A STRING NAO ESTEJA DE ACORDO COM A "SINTAXE" CORRETA 
+	// A CLASSE LANÇA UM EXCEPTION 
+	
+	
+	// ParseXxx  -> Texto para Primitivos 
+	
+	String c = "123";
+	int c1 = Integer.parseInt(c);
+	
+	// CASO A STRING NAO ESTEJA DE ACORDO COM A "SINTAXE" CORRETA 
+	// A CLASSE LANÇA UM EXCEPTION 
+	
+	
+	// XxxValue  -> Wrapper para Primitivo
+	// xxx = TIPO
+	Integer d = Integer.valueOf(10);
+	int d1 = d.intValue();
+	
+	
+	
+}
