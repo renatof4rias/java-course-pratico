35bb17639535356644337d9ce33ab5b59afc39da
renatof4rias <renatof4rias@gmail.com>
2024-03-04 11:55:57 -0300

    Aula 41

---
+++ Backend_Java_Itensivo/src/Seção_2/Aula_41_Lab.java
@@ -0,0 +1,58 @@
+package Seção_2;
+
+public class Aula_41_Lab {
+	// IMAGINE A CLASSE A SEGUIR 
+	
+	/*
+	
+	public class Pessoa{
+		
+		public String nome;     <- VARIAVEIS DE INSTANCIA
+		private int idade;		<-  
+
+	}
+
+
+
+	QUANDO EU CRIAR INSTANCIA DA CLASSE PESSOA 
+	
+	Pessoa p1 = new Pessoa();
+	Pessoa p2 = new Pessoa();
+	Pessoa p3 = new Pessoa();
+	
+
+	p1, p2, p3 TODOS VAO CONTER UMA COPIA DE VARIAVEIS, MAS OS VALORES ATRIBUIDOS A CADA OBJETOS SAO IDEPENDENTES 
+	
+	 
+	p1.nome nao tem conexos com p2.nome  sao duas referencias DISTINTAS  podem conter mesmo valores(pois há chances de duas pessoas terem o mesmo nome, mas nao sao a mesma pessoa) 
+	
+	exemplo cada objeto possui uma referencia UNICA na memoria  o objeto que ta na referencia abcd é completamente diferente do objeto na referencia efgh mas ambos vao possuir uma copia de cada atributo de classe
+	
+	p1.nome = null;
+	p2.nome = null;
+	p3.nome = null;
+	
+	se eu preencher p1.nome = "Lucas" 
+	
+	p1.nome = "Lucas";
+	p2.nome = null;
+	p3.nome = null;
+
+	se eu preencher p1.nome = "Mario" 
+
+	p1.nome = "Lucas"; REF 0x1234na
+	p2.nome = "Mario"; REF 0x109965ka
+	p3.nome = null; REF 0x412234hc
+
+
+	
+	
+
+
+
+
+
+
+
+	 */
+}
