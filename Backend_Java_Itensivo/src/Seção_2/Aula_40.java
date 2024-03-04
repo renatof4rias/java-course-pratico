f9073f4045b2be50d1b2b9979bf87a7c8774f940
renatof4rias <renatof4rias@gmail.com>
2024-03-04 11:45:42 -0300

    Aula 40

---
+++ Backend_Java_Itensivo/src/Seção_2/Aula_40.java
@@ -0,0 +1,13 @@
+package Seção_2;
+
+public class Aula_40 {
+
+	// Variaveis de INSTANCIA, sao os atributos que a classe possui
+	// Só existem dentro da classe e fora de metodos 
+	// Cada instancia de uma classe possui sua "COPIA" dessas variaveis para serem preenchidas
+	
+	
+	
+	
+	
+}
