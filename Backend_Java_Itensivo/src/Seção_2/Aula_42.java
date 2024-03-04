6605b0353db93f1c9ca9f0848ee9c4416c4992f3
renatof4rias <renatof4rias@gmail.com>
2024-03-04 16:04:58 -0300

    Aula 41 e 42

---
+++ Backend_Java_Itensivo/src/Seção_2/Aula_42.java
@@ -0,0 +1,21 @@
+package Seção_2;
+
+public class Aula_42 {
+	// VARIAVEIS DE REFERENCIA
+	// SAO VARIAVEIS QUE ARMAZENA O ENDEREÇO EM QUE O OBJETO FOI "SALVO" NA MEMORIA HEAP
+	// QUANDO INSTANCIAMOS UM OBJETO { new Pessoa() } O new FAZ A INSTANCIAÇÃO DO OBJETO NA MEMORIA E RETORNA A VARIAVEL O ENDEREÇO ONDE O OBJETO ESTÁ 
+	
+	// 
+
+	public static void main(String[] args) {
+
+		// Pessoa p1 = new Pessoa();
+		
+		// internmente p1 tem valor de referencia de onde o objeto foi instanciado 
+		// p1 = 0x12345@Pessoa 
+		
+		// na memoria tem uma "gaveta" com o endereço "0x12345@Pessoa" no qual tem um objeto com o identificador p1
+		
+	}
+
+}
