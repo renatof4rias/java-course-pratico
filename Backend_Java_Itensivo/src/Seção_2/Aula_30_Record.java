d1c89eec9670ff323bd8f601207997f9a4e62c99
renatof4rias <renatof4rias@gmail.com>
2024-03-04 21:27:16 -0300

    Aula 30

--- Backend_Java_Itensivo/src/Seção_2/Aula_30_Record.java
+++ Backend_Java_Itensivo/src/Seção_2/Aula_30_Record.java
@@ -1,10 +1,5 @@
 package Seção_2;
 
 public record Aula_30_Record(String cpf, String nome) {
- 
-	
-	
-	
-	
+
 }
- 
--- Backend_Java_Itensivo/src/Seção_2/Aula_30_Test.java
+++ Backend_Java_Itensivo/src/Seção_2/Aula_30_Test.java
@@ -7,25 +7,25 @@ public class Aula_30_Test {
 		Aula_30 a1 = new Aula_30("1231231323", "asdfghh");
 		a1.setCpf("a");
 		System.out.println(a1.getCpf());
-		
+
 		a1.setNome("a");
 		System.out.println(a1.getNome());
-		
+
 		System.out.println(a1);
-		
+
 		Aula_30_Record aa = new Aula_30_Record("1231231323", "asdfghh");
 		System.out.println(aa.cpf());
 		System.out.println(aa.nome());
-		
+
 		Integer c = 10;
-		
+
 		int a = c;
 		Integer b = a;
-		
+
 		System.out.println(a);
 		System.out.println(b);
 		System.out.println(c);
-		
+
 //		b.valueOf(a); 
 	}
 
