82383317cd0ccfcaf8017bebac3a5ea5337aa9a2
renatof4rias <renatof4rias@gmail.com>
2024-03-04 21:28:45 -0300

    Aula 39

--- Backend_Java_Itensivo/src/Seção_2/Aula_39.java
+++ Backend_Java_Itensivo/src/Seção_2/Aula_39.java
@@ -2,45 +2,37 @@ package Seção_2;
 
 public class Aula_39 {
 
-	// ValueOf  ->  Primitivo para Wrapper
+	// ValueOf -> Primitivo para Wrapper
 	// Textos e Primitivos
-	
+
 	int a = 10; // <- Primitivo
-	Integer a1 = Integer.valueOf(a);  //PRIMITIVO PARA WRAPPER
+	Integer a1 = Integer.valueOf(a); // PRIMITIVO PARA WRAPPER
 
 	String b = "123";
-	Integer b1 = Integer.valueOf(b);  // STRING PARA WRAPPER NUMERICO
+	Integer b1 = Integer.valueOf(b); // STRING PARA WRAPPER NUMERICO
 
-	// CASO A STRING NAO ESTEJA DE ACORDO COM A "SINTAXE" CORRETA 
-	// A CLASSE LANÇA UM EXCEPTION 
-	
-	
-	// ParseXxx  -> Texto para Primitivos 
-	
+	// CASO A STRING NAO ESTEJA DE ACORDO COM A "SINTAXE" CORRETA
+	// A CLASSE LANÇA UM EXCEPTION
+
+	// ParseXxx -> Texto para Primitivos
+
 	String c = "123";
 	int c1 = Integer.parseInt(c);
-	
-	// CASO A STRING NAO ESTEJA DE ACORDO COM A "SINTAXE" CORRETA 
-	// A CLASSE LANÇA UM EXCEPTION 
-	
-	
-	// XxxValue  -> Wrapper para Primitivo
+
+	// CASO A STRING NAO ESTEJA DE ACORDO COM A "SINTAXE" CORRETA
+	// A CLASSE LANÇA UM EXCEPTION
+
+	// XxxValue -> Wrapper para Primitivo
 	// xxx = TIPO
 	Integer d = Integer.valueOf(10);
 	int d1 = d.intValue();
-	
-	
-	
-	//AUTOBOXING
+
+	// AUTOBOXING
 	int e = 10;
 	Integer e1 = e;
-	
-	
-	//UNBOXING
+
+	// UNBOXING
 	Integer f = 10;
 	int f1 = f;
-	
-	
-	
-	
+
 }
