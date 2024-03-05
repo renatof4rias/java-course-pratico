113430ac8dcf923349798a3865e6243c6a0e71ec
renatof4rias <renatof4rias@gmail.com>
2024-03-04 21:26:21 -0300

    Aula 28

--- Backend_Java_Itensivo/src/Seção_2/Aula_28_Lab01.java
+++ Backend_Java_Itensivo/src/Seção_2/Aula_28_Lab01.java
@@ -4,19 +4,14 @@ public class Aula_28_Lab01 {
 
 	public static void main(String[] args) {
 
-		// DECLARANDO UMA VARIA COM O TIPO ESPECIFICO 
-		
-		
+		// DECLARANDO UMA VARIA COM O TIPO ESPECIFICO
+
 		Aula_28_Lab a1 = new Aula_28_Lab();
-		
-	// TIPO   	   NOME         CRIAMOS UMA REFERENCIA E ATRIBUIMOS A  a1
-		
-		
+
+		// TIPO NOME CRIAMOS UMA REFERENCIA E ATRIBUIMOS A a1
+
 		a1.nome = "";
-		
-		
-		
-		
+
 	}
 
 }
