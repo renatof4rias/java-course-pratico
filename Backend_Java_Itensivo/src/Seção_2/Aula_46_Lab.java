81b3abf8caf893edd91e0ea771cf0bda63c4955f
renatof4rias <renatof4rias@gmail.com>
2024-03-04 21:25:26 -0300

    Aula 27

--- Backend_Java_Itensivo/src/Seção_2/Aula_46_Lab.java
+++ Backend_Java_Itensivo/src/Seção_2/Aula_46_Lab.java
@@ -2,31 +2,22 @@ package Seção_2;
 
 public class Aula_46_Lab {
 
-	// VAR - Inferencia de tipo 
-	
+	// VAR - Inferencia de tipo
+
 	// Tipo coringa, para armazenar valores
-	// Sua utilização em Variaveis Locais 
-	// Precisa ser Inicializada 
+	// Sua utilização em Variaveis Locais
+	// Precisa ser Inicializada
 	// Seu tipo é fixo
 	// Uso em Atributos e Parametros nao sao permitidos
-	// Seu tipo é atribuido com inferencia 
-	
+	// Seu tipo é atribuido com inferencia
+
 	@SuppressWarnings("unused")
 	public void asasfas() {
-		 var idade = 0;
-		 var nome = "dez";
-		 var sexo = 'd';
-		 var altura = 1.75;
+		var idade = 0;
+		var nome = "dez";
+		var sexo = 'd';
+		var altura = 1.75;
 
-		 
-		 
 	}
-	
-	
-	
-	
-	
-	
-	
+
 }
-   
---
+++ Backend_Java_Itensivo/src/Seção_2/Aula_50_Lab.java
@@ -0,0 +1,16 @@
+package Seção_2;
+
+public class Aula_50_Lab {
+
+	private final String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public Aula_50_Lab(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+}
