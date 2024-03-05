7dc46d731943868cabe3fc68ea05ab3d3260470d
renatof4rias <renatof4rias@gmail.com>
2024-03-05 11:37:05 -0300

    Aula 52

---
+++ Backend_Java_Itensivo/src/Seção_2/Aula_54_Lab.java
@@ -0,0 +1,11 @@
+package Seção_2;
+
+public class Aula_54_Lab {
+
+	public int idade; // <-- Variavel
+	
+	public final int IDADE_1 = 10; // <-- Constante e por convensão o correto é IDADE_1
+	
+	public static final int IDADE_2 = 10; //<-- Constante Static, nao preciso do objeto para acessar 
+	
+}
