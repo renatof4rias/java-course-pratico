d975c9d005c8d1015ab92beb75c50159ed397d1a
renatof4rias <renatof4rias@gmail.com>
2024-03-05 11:01:34 -0300

    Aula 51

--- Backend_Java_Itensivo/src/Seção_2/Aula_51.java
+++ Backend_Java_Itensivo/src/Seção_2/Aula_51.java
@@ -2,4 +2,24 @@ package Seção_2;
 
 public class Aula_51 {
 
+	/*
+ 	Membros static 
+	sao membros que pertencem a classe.
+	nao há necessidade de criar uma instancia para acessar 
+	utilizados somente em Variaveis, metodos e blocos
+	 */
+	  
+	public static String nome;  
+	  
+	
+	static{
+		//bloco estatico
+	}
+	
+	
+	public static void metodoTeste() {
+		
+	}
+	
+	
 }
--- Backend_Java_Itensivo/src/Seção_2/Aula_51_Lab.java
+++ Backend_Java_Itensivo/src/Seção_2/Aula_51_Lab.java
@@ -2,9 +2,20 @@ package Seção_2;
 
 public class Aula_51_Lab {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//Posso criar uma instacia 
+		
+		Aula_51 a1 = new Aula_51();
+		
+		// isso me permite acessar membros NAO STATIC
+		// Mas nao há necessidade de uma instancia para acessar o STATIC
+		
+		Aula_51.nome = "a";
+		Aula_51.metodoTeste();
+		
+		
 	}
 
 }
