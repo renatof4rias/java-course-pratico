443ffc7bb4601490b7a9f0b31319773223ca8622
renatof4rias <renatof4rias@gmail.com>
2024-03-04 21:26:06 -0300

    Aula 28

--- Backend_Java_Itensivo/src/Seção_2/Aula_28_Lab.java
+++ Backend_Java_Itensivo/src/Seção_2/Aula_28_Lab.java
@@ -3,13 +3,9 @@ package Seção_2;
 public class Aula_28_Lab {
 
 	// CRIANDO UM TIPO ESPECIFICO DE VARIAVEIS
-	
+
 	public String nome;
 	public int idade;
 	public double Altura;
-	
-	
-	
-	
-	
-} 
+
+}
