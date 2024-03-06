71546d3ab5bcaa555d2f99d151620d4c0385bffb
renatof4rias <renatof4rias@gmail.com>
2024-03-05 22:26:00 -0300

    Aula 63

---
+++ Backend_Java_Itensivo/src/Seção_2/Aula_63.java
@@ -0,0 +1,13 @@
+package Seção_2;
+
+public class Aula_63 {
+
+	public final int somaInt() {
+		return 0;
+	}
+
+	public int somaInt(int a) {
+		return 0;
+	}
+
+}
---
+++ Backend_Java_Itensivo/src/Seção_2/Aula_63_Lab.java
@@ -0,0 +1,5 @@
+package Seção_2;
+
+public class Aula_63_Lab extends Aula_63 {
+
+}
