69268977084167cb916a66b5358b504b790076de
renatof4rias <renatof4rias@gmail.com>
2024-03-05 21:32:12 -0300

    Aula 59

---
+++ Backend_Java_Itensivo/src/Seção_2/Aula_59.java
@@ -0,0 +1,28 @@
+package Seção_2;
+
+public class Aula_59 {
+
+	// SOBRECARGA
+	
+	// REPETIR O MESMO NOME EM VARIOS METODOS COM ASSINATURAS/FUNÇOES DIFERENTES
+	
+	public int somar() {
+		return 0;
+	}
+	 
+	public int somar(int a) {
+		return 0;
+	}
+	
+	public int somar(int a, int b) {
+		return 0;
+	}
+	
+	public int somar(int a, int b, int c) {
+		return 0;
+	}
+	
+	public double somar(double a) {
+		return 0.0;
+	}
+}
