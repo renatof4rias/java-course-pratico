b3e2b39e9446c9b1fa479efae302094079fb95f4
renatof4rias <renatof4rias@gmail.com>
2024-03-08 16:43:59 -0300

    Missao Dominar 19

---
+++ Backend_Java_Itensivo/src/Seção_2_MissãoDominar/Calculadora.java
@@ -0,0 +1,11 @@
+package Seção_2_MissãoDominar;
+
+public class Calculadora {
+
+	
+	public static double quadrado(int numero) {
+		return Math.pow(numero, 2.0);
+	}
+	
+	
+}
---
+++ Backend_Java_Itensivo/src/Seção_2_MissãoDominar/CalculadoraTest.java
@@ -0,0 +1,12 @@
+package Seção_2_MissãoDominar;
+
+public class CalculadoraTest {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println(Calculadora.quadrado(10));
+		
+	}
+
+}
