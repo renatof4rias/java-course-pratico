5d7befb043d06e5557e9b66a81ec86785f046695
renatof4rias <renatof4rias@gmail.com>
2024-03-08 15:54:28 -0300

    Missao Dominar 15

---
+++ Backend_Java_Itensivo/src/Seção_2_MissãoDominar/CalculadoraEspecial.java
@@ -0,0 +1,13 @@
+package Seção_2_MissãoDominar;
+
+public class CalculadoraEspecial {
+
+	public int somar(int primeiroValor, int segundoValor) {
+		return primeiroValor + segundoValor;
+	}
+
+	public double somar(double primeiroValor, double segundoValor) {
+		return primeiroValor + segundoValor;
+	}
+
+}
---
+++ Backend_Java_Itensivo/src/Seção_2_MissãoDominar/CalculadoraEspecialTest.java
@@ -0,0 +1,14 @@
+package Seção_2_MissãoDominar;
+
+public class CalculadoraEspecialTest {
+
+	public static void main(String[] args) {
+
+		CalculadoraEspecial c1 = new CalculadoraEspecial();
+
+		System.out.println(c1.somar(10, 10));
+		System.out.println(c1.somar(10.0, 10.0));
+
+	}
+
+}
