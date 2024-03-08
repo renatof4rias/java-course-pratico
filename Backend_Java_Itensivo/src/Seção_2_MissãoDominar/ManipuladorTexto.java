99d5bf159a228331639e2ffe0fbdc238ca375449
renatof4rias <renatof4rias@gmail.com>
2024-03-08 12:19:15 -0300

    Missao Dominar 14

---
+++ Backend_Java_Itensivo/src/Seção_2_MissãoDominar/ManipuladorTexto.java
@@ -0,0 +1,13 @@
+package Seção_2_MissãoDominar;
+
+public class ManipuladorTexto {
+	
+	public void concatenador(String textoA, String textoB) {
+		System.out.println(textoA + textoB);
+	}
+		
+	public void concatenador(String textoA, String textoB, String textoC) {
+		System.out.println(textoA + textoB + textoC);
+	}
+	
+}
---
+++ Backend_Java_Itensivo/src/Seção_2_MissãoDominar/ManipuladorTextoTest.java
@@ -0,0 +1,16 @@
+package Seção_2_MissãoDominar;
+
+public class ManipuladorTextoTest {
+
+	public static void main(String[] args) {
+
+		ManipuladorTexto m1 = new ManipuladorTexto();
+		
+		
+		m1.concatenador("MEU NOME É", " renato");
+		m1.concatenador("OI", " eu sou", " O GOKU");
+		
+		
+	}
+
+}
