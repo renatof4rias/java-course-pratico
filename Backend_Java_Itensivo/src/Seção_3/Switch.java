229d5b6172ea0bd44d885154fe3365959135c2d1
renatof4rias <renatof4rias@gmail.com>
2024-03-09 12:08:51 -0300

    Aula 95

---
+++ Backend_Java_Itensivo/src/Seção_3/Switch.java
@@ -0,0 +1,28 @@
+package Seção_3;
+
+public class Switch {
+	public static void main(String[] args) {
+
+		int dia = 0;
+
+		switch (dia) {
+		case 1:
+			System.out.println("segunda");
+			break;
+		case 2:
+			System.out.println("terca");
+			break;
+		case 3:
+			System.out.println("quarta");
+			break;
+		case 4:
+			System.out.println("quinta");
+			break;
+		case 5:
+			System.out.println("sexta");
+			break;
+
+		}
+
+	}
+}
