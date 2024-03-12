e22590114d38a8a0f969f18757a5e19f01a6214a
renatof4rias <renatof4rias@gmail.com>
2024-03-12 17:35:32 -0300

    Missao Dominar 03

--- Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_03.java
+++ Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_03.java
@@ -6,20 +6,15 @@ public class MissaoDominar_03 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+		System.out.print("Digite Uma Palavra -> ");
+		String word = sc.nextLine();
+
+		for (int i = 0; i < word.length(); i++) {
+			System.out.println(word.charAt(i) + " " + i);
+
+		}
+
 		sc.close();
 	}
 
