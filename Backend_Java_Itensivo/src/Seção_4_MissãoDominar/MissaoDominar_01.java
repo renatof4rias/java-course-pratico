a1f7480342870e08cb4861cffc34c64e7c81d336
renatof4rias <renatof4rias@gmail.com>
2024-03-12 17:29:18 -0300

    Missao Dominar 02

--- Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_01.java
+++ Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_01.java
@@ -6,24 +6,13 @@ public class MissaoDominar_01 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		
-		for(int i = 1; i <= 10; i++ ){
-			
+
+		for (int i = 1; i <= 10; i++) {
+
 			System.out.println(i);
-			
+
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 		sc.close();
 	}
 
--- Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_02.java
+++ Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_02.java
@@ -6,20 +6,13 @@ public class MissaoDominar_02 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+		for (int i = 10; i >= 1; i--) {
+
+			System.out.println(i);
+
+		}
+
 		sc.close();
 	}
 
