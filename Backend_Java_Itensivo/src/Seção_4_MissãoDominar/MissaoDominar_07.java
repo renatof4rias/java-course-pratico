9f31c04b10c2f06dfe35281316e9f4892a259e67
renatof4rias <renatof4rias@gmail.com>
2024-03-12 19:57:24 -0300

    Missao Dominar 07

--- Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_07.java
+++ Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_07.java
@@ -5,25 +5,31 @@ import java.util.Scanner;
 public class MissaoDominar_07 {
 
 	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		System.out.print("Digite um Texto -> ");
+		String texto = sc.nextLine();
+
+		System.out.println(contarVogais(texto));
+
 		sc.close();
 	}
 
+	public static int contarVogais(String palavra) {
+
+		int contador = 0;
+		int index = palavra.length() - 1;
+
+		for (int i = 0; i <= index; i++) {
+
+			if (palavra.charAt(i) == 'a' || palavra.charAt(i) == 'e' || palavra.charAt(i) == 'i'
+					|| palavra.charAt(i) == 'o' || palavra.charAt(i) == 'u') {
+				contador += 1;
+			}
+
+		}
+
+		return contador;
+	}
 
 }
