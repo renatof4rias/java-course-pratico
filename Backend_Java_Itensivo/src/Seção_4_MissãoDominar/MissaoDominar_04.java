799d17dd94b6a641f0adf0fa10edd07f934c9e55
renatof4rias <renatof4rias@gmail.com>
2024-03-12 17:47:32 -0300

    Missao Dominar 05

--- Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_04.java
+++ Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_04.java
@@ -6,20 +6,19 @@ public class MissaoDominar_04 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Digite um Numero -> ");
+		int numero = sc.nextInt();
+
+		if (numero % 2 == 0) {
+			System.out.println("PAR");
+		} else {
+			System.out.println("IMPAR");
+		}
 		
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 		sc.close();
 	}
 
--- Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_05.java
+++ Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_05.java
@@ -6,20 +6,16 @@ public class MissaoDominar_05 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+		int somador = 1;
+
+		for (int contador = 1; contador <= 10; contador++) {
+
+			System.out.println(somador);
+			somador += contador;
+
+		}
+
 		sc.close();
 	}
 
