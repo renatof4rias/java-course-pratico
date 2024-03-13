61c39060c99d9a8f590f864ff47ca4e34e91ac8e
renatof4rias <renatof4rias@gmail.com>
2024-03-12 21:20:37 -0300

    Missao Dominar 08

--- Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_08.java
+++ Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_08.java
@@ -5,25 +5,33 @@ import java.util.Scanner;
 public class MissaoDominar_08 {
 
 	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		System.out.print("Texto -> ");
+		String string = sc.nextLine();
+
+		System.out.println(converterCamelCase(string));
+
 		sc.close();
 	}
 
+	public static String converterCamelCase(String string) {
+
+		int index = string.length() - 1;
+
+		String stringReturn = "";
+
+		for (int i = 0; i <= index; i++) {
+
+			if (Character.isUpperCase(string.charAt(i))) {
+				stringReturn = stringReturn + " " + string.charAt(i);
+			} else if (Character.isLowerCase(string.charAt(i))) {
+				stringReturn = stringReturn + string.charAt(i);
+			}
+
+		}
+
+		return stringReturn;
+	}
 
 }
