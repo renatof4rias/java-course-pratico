294462c10a7a3a7f0dbdf333c13373f64e93af4f
renatof4rias <renatof4rias@gmail.com>
2024-03-12 22:20:57 -0300

    Missao Dominar 11

--- Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_11.java
+++ Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_11.java
@@ -5,25 +5,31 @@ import java.util.Scanner;
 public class MissaoDominar_11 {
 
 	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		System.out.print("Digite Valor de A -> ");
+		int a = sc.nextInt();
+
+		System.out.print("Digite Valor de B -> ");
+		int b = sc.nextInt();
+
+		System.out.println("Maior = " + encontrarMaiorNumero(a, b));
+
 		sc.close();
 	}
 
+	public static int encontrarMaiorNumero(int a, int b) {
+
+		int maior = 0;
+
+		if (a > b) {
+			maior = a;
+		} else if (b > a) {
+			maior = b;
+		}
+
+		return maior;
+
+	}
 
 }
