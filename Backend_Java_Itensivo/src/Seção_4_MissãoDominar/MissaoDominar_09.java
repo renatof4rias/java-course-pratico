a205f088c973f8a3f1de43fb5d9b9a367ce4a3cd
renatof4rias <renatof4rias@gmail.com>
2024-03-12 21:36:36 -0300

    Missao Dominar 09

--- Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_09.java
+++ Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_09.java
@@ -5,25 +5,29 @@ import java.util.Scanner;
 public class MissaoDominar_09 {
 
 	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		System.out.print("Texto A -> ");
+		String textoA = sc.nextLine();
+
+		System.out.print("Texto B -> ");
+		String textoB = sc.nextLine();
+
+		System.out.println(substituirPalavra(textoA, textoB));
+
 		sc.close();
 	}
 
+	public static String substituirPalavra(String stringA, String stringB) {
+
+		String stringC = stringA;
+		stringA = stringB;
+		stringB = stringC;
+
+		return String.format("""
+				ANTES:  TextoA = %s TextoB = %s
+				DEPOIS: TextoA = %s TextoB = %s
+				""", stringB, stringA, stringA, stringB);
+	}
 
 }
