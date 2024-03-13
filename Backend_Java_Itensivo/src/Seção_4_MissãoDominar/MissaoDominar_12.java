1832ecfd189a60ce356d57665047259cbad3e34a
renatof4rias <renatof4rias@gmail.com>
2024-03-12 22:44:11 -0300

    Missao Dominar 12

--- Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_12.java
+++ Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_12.java
@@ -5,25 +5,22 @@ import java.util.Scanner;
 public class MissaoDominar_12 {
 
 	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		int contador = 1;
+		int soma = 0;
+
+		while (contador <= 100) {
+
+			if (contador % 2 != 0) {
+				soma += contador;
+			}
+			contador++;
+		}
+
+		System.out.println("Soma Total = " + soma);
 		
 		sc.close();
 	}
-
 
 }
--- Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_13.java
+++ Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_13.java
@@ -5,25 +5,30 @@ import java.util.Scanner;
 public class MissaoDominar_13 {
 
 	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		String password = "1234";
+
+		System.out.print("Senha -> ");
+		String senha = sc.nextLine();
+
+		boolean isOk = true;
+
+		do {
+
+			if (senha.equals(password)) {
+				System.out.println("Acesso Permitido");
+				isOk = false;
+			} else if (!senha.equals(password)) {
+				System.out.println("Senha Incorreta, Insira Novamente");
+				System.out.print("Senha -> ");
+				senha = sc.nextLine();
+				isOk = true;
+			}
+
+		} while (isOk == true);
+
 		sc.close();
 	}
-
 
 }
