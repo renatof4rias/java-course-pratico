c78750bf0d1bfcd42d3cac47bf16d5e132f6f2ce
renatof4rias <renatof4rias@gmail.com>
2024-03-12 23:51:09 -0300

    Missao Dominar 16

--- Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_16.java
+++ Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_16.java
@@ -5,25 +5,23 @@ import java.util.Scanner;
 public class MissaoDominar_16 {
 
 	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		System.out.print("Valor -> ");
+		int numero = sc.nextInt();
+
+		int soma = 0;
+
+		while (numero != -1) {
+			soma += numero;
+			System.out.print("Valor -> ");
+			numero = sc.nextInt();
+
+		}
+
+		System.out.println("Total " + soma);
+
 		sc.close();
 	}
-
 
 }
