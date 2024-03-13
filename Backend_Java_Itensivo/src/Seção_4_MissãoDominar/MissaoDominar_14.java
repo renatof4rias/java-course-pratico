a77c3fb29095a7c919ef4b345b5bc179445e4813
renatof4rias <renatof4rias@gmail.com>
2024-03-12 23:45:58 -0300

    Missao Dominar 14 e 15

--- Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_14.java
+++ Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_14.java
@@ -5,25 +5,33 @@ import java.util.Scanner;
 public class MissaoDominar_14 {
 
 	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		System.out.print("Digite uma Palavra -> ");
+		String palavra = sc.nextLine();
+
+		while (!palavra.equalsIgnoreCase("sair")) {
+
+			String ignoreCase = palavra.toLowerCase();
+			int contador = 0;
+
+			for (int i = 0; i <= ignoreCase.length() - 1; i++) {
+				
+				if (ignoreCase.charAt(i) == 'a' || ignoreCase.charAt(i) == 'e' || ignoreCase.charAt(i) == 'i'
+						|| ignoreCase.charAt(i) == 'o' || ignoreCase.charAt(i) == 'u') {
+				
+					contador += 1;
+
+				}
+
+			}
+			System.out.println("Total de Vogais = " + contador);
+			System.out.print("Digite uma Palavra ou Digite Sair para Encerrar -> ");
+			palavra = sc.nextLine();
+
+		}
+
 		sc.close();
 	}
-
 
 }
--- Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_15.java
+++ Backend_Java_Itensivo/src/Seção_4_MissãoDominar/MissaoDominar_15.java
@@ -5,25 +5,20 @@ import java.util.Scanner;
 public class MissaoDominar_15 {
 
 	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		System.out.print("Digit um Numero ->");
+		String numero = sc.nextLine();
+
+		int soma = 0;
+
+		for (int i = 0; i < numero.length(); i++) {
+			soma += Character.getNumericValue(numero.charAt(i));
+		}
+
+		System.out.println("Soma dos Algarismos = " + soma);
+
 		sc.close();
 	}
-
 
 }
