c3febb20c5611605709bc0b0f2b5b91e377d417f
renatof4rias <renatof4rias@gmail.com>
2024-03-15 15:58:09 -0300

    Missao Dominar 07, 08

--- Backend_Java_Itensivo/src/Seção_5_MissãoDominar/MissaoDominar_07.java
+++ Backend_Java_Itensivo/src/Seção_5_MissãoDominar/MissaoDominar_07.java
@@ -7,10 +7,24 @@ public class MissaoDominar_07 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		
+		char[] array = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+				't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+		int contadorVogais = 0;
+		int contadorConsoantes = 0;
+		for (int i = 0; i < array.length; i++) {
+
+			if (array[i] == 'a' || array[i] == 'e' || array[i] == 'i' || array[i] == 'o' || array[i] == 'u') {
+				contadorVogais++;
+			} else {
+				contadorConsoantes++;
+			}
+		}
+
+		System.out.println("Total Vogais = " + contadorVogais);
+		System.out.println("Total Consoantes = " + contadorConsoantes);
 
 		sc.close();
 	}
-
 
 }
--- Backend_Java_Itensivo/src/Seção_5_MissãoDominar/MissaoDominar_08.java
+++ Backend_Java_Itensivo/src/Seção_5_MissãoDominar/MissaoDominar_08.java
@@ -7,11 +7,34 @@ public class MissaoDominar_08 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		
+		int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 5, 5, 5, 5, 5, 5, 10, 12, 432, 52, 456, 3112, 123, 53452,
+				64, 567, 234, 5345, 62345, 1234, 5, -1 };
+
+		boolean temRepetidos = false;
+
+		for (int i = 0; i < array.length; i++) {
+
+			for (int j = i + 1; j < array.length; j++) {
+
+				if (array[i] == array[j]) {
+					temRepetidos = true;
+					break;
+				}
+
+			}
+
+			if (temRepetidos) {
+				break;
+			}
+		}
+
+		if (temRepetidos) {
+			System.out.println("O array contém valores repetidos.");
+		} else {
+			System.out.println("O array não contém valores repetidos.");
+		}
 
 		sc.close();
 	}
-
-	
 
 }
