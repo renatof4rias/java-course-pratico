20165345a58530333195bcfb452002c59b082c4d
renatof4rias <renatof4rias@gmail.com>
2024-03-15 11:20:32 -0300

    Missao Dominar 03, 04

--- Backend_Java_Itensivo/src/Seção_5_MissãoDominar/MissaoDominar_03.java
+++ Backend_Java_Itensivo/src/Seção_5_MissãoDominar/MissaoDominar_03.java
@@ -7,7 +7,32 @@ public class MissaoDominar_03 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		 
+		int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 432, 52, 456, 3112, 123, 53452, 64, 567, 234, 5345,
+				62345, 1234, 5, -1 };
+
+		int maior = array[0];
+		int menor = array[0];
+
+		for (int i = 0; i < array.length; i++) {
+
+			if (array[i] > maior) {
+				maior = array[i];
+			}
+
+		}
+
+		for (int i = 0; i < array.length; i++) {
+
+			if (array[i] < menor) {
+				menor = array[i];
+			}
+
+		}
+
+		System.out.println("Maior Valor = " + maior);
+
+		System.out.println("Menor Valor = " + menor);
+
 		sc.close();
 	}
 
--- Backend_Java_Itensivo/src/Seção_5_MissãoDominar/MissaoDominar_04.java
+++ Backend_Java_Itensivo/src/Seção_5_MissãoDominar/MissaoDominar_04.java
@@ -7,8 +7,21 @@ public class MissaoDominar_04 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		 
+		int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 5, 5, 5, 5, 5, 5, 10, 12, 432, 52, 456, 3112, 123, 53452, 64, 567, 234, 5345,
+				62345, 1234, 5, -1 };
 
+		int contador = 0;
+
+		for (int i = 0; i < array.length; i++) {
+
+			if (array[i] == 5) {
+				contador++;
+			}
+
+		}
+
+System.out.println("O Numero 5 Apareceu " + contador + " Vezes");
+		
 		sc.close();
 	}
 
