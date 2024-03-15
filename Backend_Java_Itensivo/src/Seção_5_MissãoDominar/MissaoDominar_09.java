4f87ab96b8d50f2f7f93e9600097b0d509511f18
renatof4rias <renatof4rias@gmail.com>
2024-03-15 16:13:50 -0300

    Missao Dominar 09, 10

--- Backend_Java_Itensivo/src/Seção_5_MissãoDominar/MissaoDominar_09.java
+++ Backend_Java_Itensivo/src/Seção_5_MissãoDominar/MissaoDominar_09.java
@@ -1,11 +1,26 @@
 package Seção_5_MissãoDominar;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MissaoDominar_09 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 5, 5, 5, 5, 5, 5, 10, 12, 432, 52, 456, 3112, 123, 53452,
+				64, 567, 234, 5345, 62345, 1234, 5, -1 };
+
+		System.out.println("Antes  " + Arrays.toString(array));
+
+		for (int i = 0; i < array.length; i++) {
+
+			if (array[i] % 2 == 0) {
+				array[i] = 0;
+			}
+
+		}
+
+		System.out.println("Depois " + Arrays.toString(array));
 
 		sc.close();
 	}
--- Backend_Java_Itensivo/src/Seção_5_MissãoDominar/MissaoDominar_10.java
+++ Backend_Java_Itensivo/src/Seção_5_MissãoDominar/MissaoDominar_10.java
@@ -7,8 +7,24 @@ public class MissaoDominar_10 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
+		String[] palavras = new String[] { "Caixa", "Lapis", "Computador", "aqwewrwedfadfae", "Macarronada", "Caneta",
+				"Celular", "Telefone", "Garrafa", "Agua", "Livro", "Funcionario", "" };
+
+		int tamanho = 0;
+		String maiorPalavra = "";
+
+		for (int i = 0; i < palavras.length; i++) {
+
+			if (palavras[i].length() >= tamanho) {
+				tamanho = palavras[i].length();
+				maiorPalavra = palavras[i];
+			}
+
+		}
+
+		System.out.println("Maior Palavra é -> " + maiorPalavra + " com " + tamanho + " Letras");
+
 		sc.close();
 	}
-
 
 }
